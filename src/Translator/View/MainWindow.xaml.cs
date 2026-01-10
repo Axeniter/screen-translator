71c032b2d5a9d9e6b3e4311101ca2c1e524b075b
Axeniter <anamievoid@gmail.com>
2026-01-10 18:43:12 +1000

    Feat: select languages in main window
    add language selection
    add simple viewmodel for main window

--- src/Translator/View/MainWindow.xaml.cs
+++ src/Translator/View/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Translator.ViewModel;
 
 namespace Translator.View
 {
@@ -19,6 +11,7 @@ namespace Translator.View
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
         }
     }
 }
