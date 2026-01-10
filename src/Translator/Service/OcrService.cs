0730ece90c9a833b7228be3398a5268a493e0888
Axeniter <anamievoid@gmail.com>
2026-01-10 14:38:00 +1000

    Feat: ocr language class

---
+++ src/Translator/Service/OcrService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translator.Service
+{
+    public class OcrLanguage
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public bool IsInstalled { get ; set; }
+        public long SizeInBytes { get; set; }
+        public string DownladUrl { get; set; }
+
+    }
+
+    public class OcrService
+    {
+    }
+}
