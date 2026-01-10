93b703cd7659ec4bd4c8d396a611c72fcc81f4ba
Axeniter <anamievoid@gmail.com>
2026-01-10 15:09:56 +1000

    Feat: ocr service

--- src/Translator/Model/OcrLanguage.cs
+++ src/Translator/Model/OcrLanguage.cs
@@ -5,7 +5,6 @@
         public string Code { get; set; }
         public string Name { get; set; }
         public bool IsInstalled { get; set; }
-        public long SizeInBytes { get; set; }
         public string DownladUrl { get; set; }
 
     }
--- src/Translator/Service/OcrService.cs
+++ src/Translator/Service/OcrService.cs
@@ -1,7 +1,39 @@
-﻿namespace Translator.Service
+﻿using System.Drawing;
+using System.IO;
+using Tesseract;
+
+namespace Translator.Service
 {
-    public class OcrService
+    public class OcrService : IDisposable
     {
+        private readonly string _tessDataPath;
+        private readonly Dictionary<string, TesseractEngine> _engines = new();
+
+        public OcrService()
+        {
+            var appData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            _tessDataPath = Path.Combine(appData, "Translator", "tessdata");
+            if (!Directory.Exists(_tessDataPath)) Directory.CreateDirectory(_tessDataPath);
+        }
+        public string RecognizeImage(Bitmap image, string language)
+        {
+            if (!_engines.ContainsKey(language))
+            {
+                var engine = new TesseractEngine(_tessDataPath, language, EngineMode.Default);
+                _engines[language] = engine;
+            }
+
+            using (var pix = PixConverter.ToPix(image))
+            using (var page = _engines[language].Process(pix))
+            {
+                return page.GetText();
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (var engine in _engines.Values) engine.Dispose();
+        }
 
     }
 }
