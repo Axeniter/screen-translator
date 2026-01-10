b5263dde7635513499e539f17d4b02b1f61eee0e
Axeniter <anamievoid@gmail.com>
2026-01-10 20:04:52 +1000

    Feat: translation window

---
+++ src/Translator/View/TranslationWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Translator.View
+{
+    /// <summary>
+    /// Interaction logic for TranslationWindow.xaml
+    /// </summary>
+    public partial class TranslationWindow : Window
+    {
+        public TranslationWindow(string translatedText)
+        {
+            InitializeComponent();
+            TranslatedText.Text = translatedText;
+        }
+
+        public TranslationWindow()
+        {
+            InitializeComponent();
+            TranslatedText.Text = "no text";
+        }
+    }
+}
