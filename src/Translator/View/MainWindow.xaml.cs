7d02c943c92b8b27b3f31cc50d9d85d35abbf38a
Axeniter <anamievoid@gmail.com>
2026-01-10 18:07:01 +1000

    Feat: main window base ui

--- src/Translator/View/MainWindow.xaml.cs
+++ src/Translator/View/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Translator
+namespace Translator.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
