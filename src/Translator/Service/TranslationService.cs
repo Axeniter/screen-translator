c9224545c1adf17b7990870f2ad15c1a19860508
Axeniter <anamievoid@gmail.com>
2026-01-11 14:16:16 +1000

    Feat: translation service

---
+++ src/Translator/Service/TranslationService.cs
@@ -0,0 +1,26 @@
+﻿using GTranslate.Translators;
+
+namespace Translator.Service
+{
+    public class TranslationService
+    {
+        private GoogleTranslator2 _translator = new();
+
+        public async Task<string?> TranslateTextAsync(string text, string language)
+        {
+            string languageCode;
+            try
+            {
+                languageCode = LanguageService.GetTranslationLanguageCode(language);
+            }
+            catch
+            {
+                return null;
+            }
+
+            var result = await _translator.TranslateAsync(text, languageCode);
+
+            return result.Translation;
+        }
+    }
+}
