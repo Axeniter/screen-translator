9d842bf5151c5e97f27a90add1877f820ec6eacf
Axeniter <anamievoid@gmail.com>
2026-01-11 22:38:49 +1000

    Feat: viewmodel process translation from bitmap support

--- src/Translator/ViewModel/MainWindowViewModel.cs
+++ src/Translator/ViewModel/MainWindowViewModel.cs
@@ -1,17 +1,24 @@
 ﻿using ReactiveUI;
 using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Windows;
 using Translator.Service;
 
 namespace Translator.ViewModel
 {
     public class MainWindowViewModel : ReactiveObject
     {
+        private readonly OcrService _ocrService = new();
+        private readonly OcrLanguageManager _ocrLanguageManager = new();
+        private readonly TranslationService _translationService = new();
+
         private string? _selectedSourceLanguage = null;
         private string? _selectedTargetLanguage = null;
         private string? _hotkey = "non assigned";
 
         public ObservableCollection<string> SourceLanguages { get; } = new(LanguageService.GetAllOcrLanguages());
         public ObservableCollection<string> TargetLanguages { get; } = new(LanguageService.GetAllTranslationLanguages());
+
 
         public string? SelectedSourceLanguage
         { 
@@ -31,10 +38,23 @@ namespace Translator.ViewModel
             set => this.RaiseAndSetIfChanged(ref _hotkey, value);
         }
 
-
-        public MainWindowViewModel()
+        public async Task<string?> TranslateFromBitmapAsync(Bitmap bitmap)
         {
+            if (SelectedSourceLanguage == null) return null;
+            if (SelectedTargetLanguage == null) return null;
+            if (bitmap == null) throw new ArgumentNullException();
+            if (!_ocrLanguageManager.IsLanguageInstalled(SelectedSourceLanguage))
+            {
+                MessageBox.Show("Language not installed");
+            }
 
+            var sourceText = _ocrService.RecognizeImage(bitmap, SelectedSourceLanguage);
+
+            if (string.IsNullOrEmpty(sourceText)) return null;
+
+            var translation = await _translationService.TranslateTextAsync(sourceText, SelectedTargetLanguage);
+
+            return translation;
         }
 
     }
